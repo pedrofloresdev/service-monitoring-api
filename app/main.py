0e62d49dbd6e6a4b1bf84fb7bf0f6ddc190e3b67
Pedro Flores <manuelflores1795@gmail.com>
2026-03-19 17:18:08 -0500

    feat: initial commit project setup

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/health")
+def read_health():
+    return {"status": "ok"}
